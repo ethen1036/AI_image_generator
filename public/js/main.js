a94ed25ac992b4c7f338f857e1008e408e0fe8ee
Shubham Kumar <sraj94710@gmail.com>
2022-12-08 15:07:04 +0530

    complete project

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+
+function onSubmit(e){
+    e.preventDefault();
+
+    document.querySelector('.msg').textContent='';
+    document.querySelector('#image').src='';
+
+    const prompt = document.querySelector('#prompt').value;
+    const size = document.querySelector('#size').value;
+
+    if(prompt === ''){
+        alert('Please add some text');
+        return;
+    }
+
+    generateImageRequest(prompt, size);
+}
+
+async function generateImageRequest(prompt, size){
+    try {
+        showSpinner();
+
+        const response = await fetch('/openai/generateimage', {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({prompt, size})
+        });
+
+        if(!response.ok){
+            hideSpinner();
+            throw new Error('The image could not be generated');
+        }
+
+        const data = await response.json();
+        console.log(data);
+        const imageUrl = data.data;
+
+        document.querySelector('#image').src=imageUrl;
+        hideSpinner();
+
+    } catch (error) {
+        document.querySelector('.msg').textContent=error;
+    }
+}
+
+function showSpinner(){
+    document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner(){
+    document.querySelector('.spinner').classList.remove('show');
+}
+
+document.querySelector('#image_form').addEventListener('submit', onSubmit);
